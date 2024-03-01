8716cc308ad22edd7236083a744558a8fa4a70ee
kaouther <kaoutherboutheldja@gmail.com>
2024-03-01 13:56:24 +0100

    initialize project

---
+++ space/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './components/navBar/navBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      
+    </>
+  )
+}
+
+export default App
---
+++ space/src/components/navBar/navBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function navBar() {
+  return (
+    <nav>
+      <img src='' alt='star'/>
+
+    </nav>
+  )
+}
